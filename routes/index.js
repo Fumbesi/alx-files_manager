5c5525ce934b33b35f874bdafefb1f10316fbc0a
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-03-11 14:02:25 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,21 @@
-i
+/* eslint-disable */
+import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
+
+function injectRoutes(app) {
+  app.get('/status', AppController.getStatus);
+  app.get('/stats', AppController.getStats);
+  app.post('/users', UsersController.postNew);
+  app.get('/connect', AuthController.getConnect);
+  app.get('/disconnect', AuthController.getDisconnect);
+  app.get('/users/me', UsersController.me);
+  app.post('/files', FilesController.postUpload);
+  app.get('/files', FilesController.getIndex);
+  app.get('/files/:id', FilesController.getShow);
+  app.put('/files/:id/publish', FilesController.putPublish);
+  app.put('/files/:id/unpublish', FilesController.putUnpublish)
+}
+
+export default injectRoutes;
