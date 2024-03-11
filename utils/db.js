b967b96766f8c9d4ed5b7bbe189a6f4b3f059d8c
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-03-11 14:03:04 +0200

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,85 @@
-iu
+import mongodb from 'mongodb';
+/* eslint-disable */
+
+/**
+ * Represents a MongoDB client.
+ */
+class DBClient {
+  /**
+   * Creates a new DBClient instance.
+   */
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  /**
+   * Checks if this client's connection to the MongoDB server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  /**
+   * Retrieves the number of users in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  /**
+   * Retrieves the number of files in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+
+  async findBy(obj, coll) {
+    if (typeof obj != 'object'){
+      return new Error('search values must be an object');
+    }
+
+    return this.client.db().collection(coll).find(obj)
+  }
+
+  async findByPag(obj, coll, page) {
+    if (typeof obj != 'object'){
+      return new Error('search values must be an object');
+    }
+    return this.client.db().collection(coll).find(obj).limit(20).skip(Number(page));
+  }
+
+
+  async insertDB(doc, coll) {
+    if (typeof doc != 'object'){
+      return new Error('document must be an object');
+    }
+    return this.client.db().collection(coll).insertOne(doc);
+  }
+
+  async updateDB(doc, upDoc, coll) {
+    if (typeof doc != 'object'){
+      return new Error('document must be an object');
+    }
+    return this.client.db().collection(coll).findOneAndUpdate(doc, upDoc, {returnDocument: 'after'});
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
