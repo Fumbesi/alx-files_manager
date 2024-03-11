6269105cadee0f1ddb98070d60d9cbba0e6573b8
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-03-11 13:55:57 +0200

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,25 @@
-iu
+/*eslint-disable */
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+export default class AppController {
+
+    static getStatus(_req, res){
+        let redis = false;
+        let db = false;
+        if (redisClient.isAlive()) {
+            redis = true;
+        }
+        if (dbClient.isAlive()) {
+            db = true;
+        }
+        res.json({redis, db});
+    }
+    
+    static getStats(_req, res) {
+        Promise.all([dbClient.nbFiles(), dbClient.nbUsers()])
+        .then(([files, users]) => {
+            res.json({users, files})
+        })
+    }
+}
